e4b1371e4aba53e3dfe626e1d5eebbabf31f3065
AkankshaCodes <116874997+AkankshaT0@users.noreply.github.com>
2026-07-23 00:16:31 +0530

    Time: 5 ms (61.48%), Space: 44.5 MB (67.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -6,19 +6,29 @@ class Solution {
             return false;
         }
 
+        // Frequency array for 26 lowercase English letters
         int[] freq = new int[26];
 
+        // Traverse both strings
         for(int i = 0; i < s.length(); i++){
 
+            // Increase frequency of character from s
             freq[s.charAt(i) - 'a']++;
+
+            // Decrease frequency of character from t
             freq[t.charAt(i) - 'a']--;
         }
 
+        // Check if all frequencies are zero
         for(int count : freq){
+
+            // Non-zero means character frequencies don't match
             if(count != 0){
                 return false;
             }
         }
+
+        // All character frequencies matched
         return true;
     }
 }
