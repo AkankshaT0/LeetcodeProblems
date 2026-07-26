cdcc20899e821ecbfeaed3c3bf1342de7ddb800b
AkankshaCodes <116874997+AkankshaT0@users.noreply.github.com>
2026-07-26 22:03:51 +0530

    Time: 42 ms (65.95%), Space: 53.4 MB (70.78%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -3,12 +3,17 @@
  * @param {Function} fn
  * @return {number[]}
  */
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
 var map = function(arr, fn) {
-    const returnArray = [];
+    const result = new Array(arr.length);
 
-    for(let i = 0; i < arr.length; i++){
-        returnArray.push(fn(arr[i],i));
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = fn(arr[i], i);
     }
 
-    return returnArray;
+    return result;
 };
