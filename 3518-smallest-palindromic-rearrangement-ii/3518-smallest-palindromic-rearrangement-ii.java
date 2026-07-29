74167200fcf1411d09f7e2d9a3885bd4c5a33ff6
Akankshaa <116874997+AkankshaT0@users.noreply.github.com>
2026-07-29 21:41:56 +0530

    Time: 23 ms (74.19%), Space: 47.8 MB (12.9%) - LeetHub

--- 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.java
+++ 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.java
@@ -1,103 +1,63 @@
 class Solution {
+    long C(int n, int r, long k) {
+        r = Math.min(r, n - r);
+        long ans = 1;
+
+        for (int i = 1; i <= r; i++) {
+            ans = ans * (n - r + i) / i;
+            if (ans >= k) return k;
+        }
+        return ans;
+    }
+
+    long ways(int[] f, long k) {
+        long ans = 1;
+        int n = 0;
+
+        for (int x : f) {
+            if (x > 0) {
+                ans *= C(n + x, x, k);
+                if (ans >= k) return k;
+                n += x;
+            }
+        }
+        return ans;
+    }
+
     public String smallestPalindrome(String s, int k) {
-        int n = s.length();
-        int half = n / 2;
+        int[] f = new int[26];
+        for (char c : s.toCharArray())
+            f[c - 'a']++;
 
-        int[] freq = new int[26];
-        for (char c : s.toCharArray()) {
-            freq[c - 'a']++;
-        }
+        for (int i = 0; i < 26; i++)
+            f[i] /= 2;
 
-        // Keep only characters needed for the left half
-        for (int i = 0; i < 26; i++) {
-            freq[i] /= 2;
-        }
+        StringBuilder a = new StringBuilder();
 
-        // If total number of palindromes < k
-        if (count(freq, half, k) < k) {
-            return "";
-        }
-
-        StringBuilder left = new StringBuilder();
-
-        // Construct the k-th lexicographically smallest left half
-        for (int pos = 0; pos < half; pos++) {
-
+        for (int p = 0; p < s.length() / 2; p++) {
             for (int c = 0; c < 26; c++) {
+                if (f[c] == 0) continue;
 
-                if (freq[c] == 0) continue;
+                f[c]--;
+                long cnt = ways(f, k);
 
-                // Try placing character c
-                freq[c]--;
-
-                long ways = count(freq, half - pos - 1, k);
-
-                if (ways >= k) {
-                    // k-th answer is inside this group
-                    left.append((char) ('a' + c));
+                if (cnt >= k) {
+                    a.append((char)('a' + c));
                     break;
                 }
 
-                // Skip this entire group
-                k -= ways;
-                freq[c]++;
+                k -= cnt;
+                f[c]++;
             }
         }
 
-        String right = new StringBuilder(left).reverse().toString();
+        if (a.length() != s.length() / 2)
+            return "";
 
-        // Original s is already palindrome,
-        // so the middle character is s[n / 2]
-        if (n % 2 == 1) {
-            return left.toString() + s.charAt(n / 2) + right;
-        }
+        String b = new StringBuilder(a).reverse().toString();
 
-        return left.toString() + right;
-    }
-
-    // Count distinct permutations, but cap answer at k
-    private long count(int[] freq, int remaining, long k) {
-        long ways = 1;
-        int used = 0;
-
-        for (int f : freq) {
-            if (f == 0) continue;
-
-            // Choose positions for these f identical characters
-            long comb = nCr(used + f, f, k);
-
-            if (ways >= (k + comb - 1) / comb) {
-                return k;
-            }
-
-            ways *= comb;
-            used += f;
-        }
-
-        return ways;
-    }
-
-    // Calculate nCr while capping the result at k
-    private long nCr(int n, int r, long k) {
-        r = Math.min(r, n - r);
-
-        long res = 1;
-
-        for (int i = 1; i <= r; i++) {
-            long x = n - r + i;
-
-            // res * x / i >= k
-            if (res > k * i / x) {
-                return k;
-            }
-
-            res = res * x / i;
-
-            if (res >= k) {
-                return k;
-            }
-        }
-
-        return res;
+        return s.length() % 2 == 0
+                ? a + b
+                : a + "" + s.charAt(s.length() / 2) + b;
     }
 }
