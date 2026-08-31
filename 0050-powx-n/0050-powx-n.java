7b4b06c016bd08f7742e2e1a2aa7a0f2b3cd6d50
Akankshaa <116874997+AkankshaT0@users.noreply.github.com>
2026-08-31 18:54:33 +0530

    Time: 0 ms (100%), Space: 47.6 MB (78.45%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,25 @@
+class Solution {
+    public double myPow(double x, int n) {
+
+        long N = n;
+        if( N < 0){
+            x = 1 / x;
+            N = -N;
+        }
+        if( N == 0){
+            return 1;
+        }
+
+        double halfPower = myPow( x ,(int)(N / 2));
+
+        // if power is even
+        double halfPowerSquare = halfPower * halfPower;
+
+        // if power is odd
+        if( N % 2 != 0){
+            halfPowerSquare = x * halfPowerSquare;
+        }
+
+        return halfPowerSquare;
+    }
+}
