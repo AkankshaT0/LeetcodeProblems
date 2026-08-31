66d2ab875f678a26af280298a6aa61c360a6e2b9
Akankshaa <116874997+AkankshaT0@users.noreply.github.com>
2026-08-31 18:56:10 +0530

    Time: 0 ms (100%), Space: 48.3 MB (22.16%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -2,6 +2,8 @@ class Solution {
     public double myPow(double x, int n) {
 
         long N = n;
+
+        // negative n for 2^-3 = 1 / 2^3
         if( N < 0){
             x = 1 / x;
             N = -N;
